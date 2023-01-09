f22087734fa001cb4ed423c900a348148cc9b073
AppAMP-001 <shariothossian10112003@gmail.com>
2023-01-09 11:48:49 +0600

    second commit

--- DB/Conn.js
+++ DB/Conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'});
 mongoose.connect(process.env.DATABASE,{autoIndex:true}).then(()=>{
     console.log('MongoDB database connection successfully')
 }).catch((error)=>{
